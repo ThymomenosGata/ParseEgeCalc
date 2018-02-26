08b0301b041d4dbb8cc000178481e2bbd5f50f6d
prumin <kostaki3@yandex.ru>
2018-02-26 20:04:53 +0300

    use selenium

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
-        ParseMoney.Parser();
+        ParseEgeCalc.Parser();
     }
 }
---
+++ src/main/java/ParseEgeCalc.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParseEgeCalc {
+    private static final String URL = "http://egecalc.ru/?rus=100&mat=100&soc=100" +
+            "&phy=100&his=100&bio=100&che=100&lan=100&ict=100&geo=100&lit=100" +
+            "&sort_by=score&city=all&page=1";
+
+    public static void Parser(){
+        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+        WebElement element = driver.findElement(By.name("rus"));
+        WebElement element1 = driver.findElement(By.name("mat"));
+        WebElement element2 = driver.findElement(By.name("soc"));
+        WebElement element3 = driver.findElement(By.name("phy"));
+        WebElement element4 = driver.findElement(By.name("his"));
+        WebElement element5 = driver.findElement(By.name("bio"));
+        WebElement element6 = driver.findElement(By.name("che"));
+        WebElement element7 = driver.findElement(By.name("lan"));
+        WebElement element8 = driver.findElement(By.name("ict"));
+        WebElement element9 = driver.findElement(By.name("geo"));
+        WebElement element10 = driver.findElement(By.name("lit"));
+        element.sendKeys("100");
+        element1.sendKeys("100");
+        element2.sendKeys("100");
+        element3.sendKeys("100");
+        element4.sendKeys("100");
+        element5.sendKeys("100");
+        element6.sendKeys("100");
+        element7.sendKeys("100");
+        element8.sendKeys("100");
+        element9.sendKeys("100");
+        element10.sendKeys("100");
+        element.submit();
+        element1.submit();
+        element2.submit();
+        element3.submit();
+        element4.submit();
+        element5.submit();
+        element6.submit();
+        element7.submit();
+        element8.submit();
+        element9.submit();
+        element10.submit();
+
+        WebElement el = driver.findElement(By.className("card-block"));
+         
+        System.out.println(el.getText());
+    }
+
+
+}
